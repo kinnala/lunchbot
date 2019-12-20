217da1980f3eadadfe5bcebac955ce64a7d28c4c
Tom Gustafsson <tom.gustafsson@iki.fi>
2019-12-20 10:37:36 +0200

    Make it work on Fridays

--- run.sh
+++ run.sh
@@ -6,5 +6,12 @@ if [[ -z "${LUNCH_SECRET}" ]]; then
 	exit 1
 fi
 
-FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /'`date -d @$(($(date +%s)+84600)) +%A | sed 's/.*/\u&/'`'/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
+# Factory
+if [ `date +%A` == "perjantai" ]; then
+    FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /Monday/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
+else
+    FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /'`date -d @$(($(date +%s)+84600)) +%A | sed 's/.*/\u&/'`'/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
+fi
+
+# Send to Slack
 curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$FACTORY\"}" https://hooks.slack.com/services/$LUNCH_SECRET
