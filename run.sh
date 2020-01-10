296e7e33504abd9f7ef0f6cd6bace86642ce6503
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-01-10 11:10:44 +0200

    Add Silinteri

--- run.sh
+++ run.sh
@@ -13,5 +13,12 @@ else
     FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /'`date -d @$(($(date +%s)+84600)) +%A | sed 's/.*/\u&/'`'/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
 fi
 
+# Silinteri
+export PYTHONIOENCODING=utf8
+SILINTERI=$(curl -s 'https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=019002&language=fi' | \
+                python2 -c "import sys, json; sonni=json.load(sys.stdin); ruuat=[sonni['MenusForDays'][0]['SetMenus'][i]['Name'] + ': ' + ', '.join(sonni['MenusForDays'][0]['SetMenus'][i]['Components']) for i in range(len(sonni['MenusForDays'][0]['SetMenus']))]; print '\n'.join(ruuat)")
+
+TEXT="FACTORY:\n$FACTORY\n\nSILINTERI:\n$SILINTERI"
+
 # Send to Slack
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$FACTORY\"}" https://hooks.slack.com/services/$LUNCH_SECRET
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$TEXT\"}" https://hooks.slack.com/services/$LUNCH_SECRET
