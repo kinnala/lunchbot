f11c01df10d13d85578395c945dee05f5aa16da3
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-01-14 22:06:19 +0200

    Add Maukas

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ -z "${LUNCH_SECRET}" ]]; then
-	echo "Environment variable LUNCH_SECRET must be defined."
-	echo "Output is sent to hooks.slack.com/services/\$LUNCH_SECRET."
-	exit 1
-fi
-
 # Factory
 if [ `date +%A` == "perjantai" ]; then
     FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /Monday/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
@@ -18,7 +12,18 @@ export PYTHONIOENCODING=utf8
 SILINTERI=$(curl -s 'https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=019002&language=fi' | \
                 python2 -c "import sys, json; sonni=json.load(sys.stdin); ruuat=[sonni['MenusForDays'][0]['SetMenus'][i]['Name'] + ': ' + ', '.join(sonni['MenusForDays'][0]['SetMenus'][i]['Components']) for i in range(len(sonni['MenusForDays'][0]['SetMenus']))]; print '\n'.join(ruuat)")
 
-TEXT="FACTORY:\n$FACTORY\n\nSILINTERI:\n$SILINTERI"
+# Maukas
+MAUKAS=$(curl -s 'https://www.mau-kas.fi/' | grep "block_level bold" | sed -e 's/<[^>]*>//g' | perl -MHTML::Entities -pe 'decode_entities($_);')
+
+TEXT="FACTORY:\n$FACTORY\n\nSILINTERI:\n$SILINTERI\n\nMAUKAS:\n$MAUKAS"
+
+echo $TEXT
+
+if [[ -z "${LUNCH_SECRET}" ]]; then
+	echo "Environment variable LUNCH_SECRET must be defined."
+	echo "Output is sent to hooks.slack.com/services/\$LUNCH_SECRET."
+	exit 1
+fi
 
 # Send to Slack
 curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$TEXT\"}" https://hooks.slack.com/services/$LUNCH_SECRET
