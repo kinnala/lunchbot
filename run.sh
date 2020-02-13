202f56ef331f1a9852ef14d2c49ab0b9bd7cee96
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-02-13 11:23:23 +0200

    Split to several messages

--- run.sh
+++ run.sh
@@ -6,21 +6,21 @@ if [ `date +%A` == "perjantai" ]; then
 else
     FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /'`date -d tomorrow +%A | sed 's/.*/\u&/'`'/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
 fi
+echo $FACTORY
 
 # Silinteri
 export PYTHONIOENCODING=utf8
 SILINTERI=$(curl -s 'https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=019002&language=fi' | \
                 python2 -c "import sys, json; sonni=json.load(sys.stdin); ruuat=[sonni['MenusForDays'][0]['SetMenus'][i]['Name'] + ': ' + ', '.join(sonni['MenusForDays'][0]['SetMenus'][i]['Components']) for i in range(len(sonni['MenusForDays'][0]['SetMenus']))]; print '\n'.join(ruuat)")
+echo $SILINTERI
 
 # Maukas
 MAUKAS=$(curl -s 'https://www.mau-kas.fi/' | grep "block_level bold" | sed -e 's/<[^>]*>//g' | perl -MHTML::Entities -pe 'decode_entities($_);')
+echo $MAUKAS
 
 # Kipsari
 KIPSARI=$(WD=`date +%a` WD=$(tr [:lower:] [:upper:] <<< ${WD:0:1})${WD:1} && curl -vs http://www.kipsari.com/ 2>&1 | grep -o -E ">$WD [^<]*<" | sed "s/^>$WD \([^<]*\)<$/\1/")
-
-TEXT="FACTORY:\n$FACTORY\n\nSILINTERI:\n$SILINTERI\n\nMAUKAS:\n$MAUKAS\n\nKIPSARI:\n$KIPSARI"
-
-echo $TEXT
+echo $KIPSARI
 
 if [[ -z "${LUNCH_SECRET}" ]]; then
 	echo "Environment variable LUNCH_SECRET must be defined."
@@ -29,4 +29,7 @@ if [[ -z "${LUNCH_SECRET}" ]]; then
 fi
 
 # Send to Slack
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$TEXT\"}" https://hooks.slack.com/services/$LUNCH_SECRET
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"_FACTORY_\n$FACTORY\"}" https://hooks.slack.com/services/$LUNCH_SECRET
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"_SILINTERI_\n$SILINTERI\"}" https://hooks.slack.com/services/$LUNCH_SECRET
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"_MAUKAS_\n$MAUKAS\"}" https://hooks.slack.com/services/$LUNCH_SECRET
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"_KIPSARI_\n$KIPSARI\"}" https://hooks.slack.com/services/$LUNCH_SECRET
