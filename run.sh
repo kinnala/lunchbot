6c2c07d426d126e7915797a7a44240cabb0a3abb
Topi Kuutela <topi.kuutela@aalto.fi>
2020-02-27 14:02:51 +0200

    Fixed multiline string printing.
    Signed-off-by: Topi Kuutela <topi.kuutela@aalto.fi>

--- run.sh
+++ run.sh
@@ -6,21 +6,21 @@ if [ `date +%A` == "perjantai" ]; then
 else
     FACTORY="$(curl -vs https://ravintolafactory.com/lounasravintolat/ravintolat/espoo-otaniemi/ 2>&1 | awk '/'`date +%A | sed 's/.*/\u&/'`'/{f=1} f; /'`date -d tomorrow +%A | sed 's/.*/\u&/'`'/{f=0}' | sed -e 's/<[^>]*>//g' | sed '$d')"
 fi
-echo $FACTORY
+echo "${FACTORY}"
 
 # Silinteri
 export PYTHONIOENCODING=utf8
 SILINTERI=$(curl -s 'https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=019002&language=fi' | \
                 python2 -c "import sys, json; sonni=json.load(sys.stdin); ruuat=[sonni['MenusForDays'][0]['SetMenus'][i]['Name'] + ': ' + ', '.join(sonni['MenusForDays'][0]['SetMenus'][i]['Components']) for i in range(len(sonni['MenusForDays'][0]['SetMenus']))]; print '\n'.join(ruuat)")
-echo $SILINTERI
+echo "${SILINTERI}"
 
 # Maukas
 MAUKAS=$(curl -s 'https://www.mau-kas.fi/' | grep "block_level bold" | sed -e 's/<[^>]*>//g' | perl -MHTML::Entities -pe 'decode_entities($_);')
-echo $MAUKAS
+echo "${MAUKAS}
 
 # Kipsari
 KIPSARI=$(WD=`date +%a` WD=$(tr [:lower:] [:upper:] <<< ${WD:0:1})${WD:1} && curl -vs http://www.kipsari.com/ 2>&1 | grep -o -E ">$WD [^<]*<" | sed "s/^>$WD \([^<]*\)<$/\1/")
-echo $KIPSARI
+echo "${KIPSARI}
 
 # Pizza bar
 PIZZABAR="$(curl -s 'https://www.pizzabar.fi/lounas' | grep -B 4 Kasvis | sed '/^$/d' | perl -0777 -ne 'print "$1 $2LINEBREAK$3\n" while /[^\n]*(Ma|Ti|Ke|To|Pe)[^\n]*\n[^\n]*(Liha:[^<]*).*?(Kasvis:[^<]*)/smg' | (mapfile -t; echo "${MAPFILE[(($(date +%u)-1))]}") | cut -c 4- | sed s/LINEBREAK/\\n/)"
